f51c86f3716a66e210b60179ba735584b49ac719
Void <filipecastro2002@gmail.com>
2025-02-07 23:05:33 +0000

    Made changes to tombstone

--- src/main/java/net/_void/civilizations/mixin/TombstoneMixin.java
+++ src/main/java/net/_void/civilizations/mixin/TombstoneMixin.java
@@ -2,11 +2,17 @@ package net._void.civilizations.mixin;
 
 
 import net._void.civilizations.block.ModBlocks;
+import net._void.civilizations.block.entity.TombstoneBlockEntity;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -19,19 +25,30 @@ import net._void.civilizations.block.custom.TombstoneBlock;
 
 import java.util.Objects;
 
-@Mixin(PlayerEntity.class)
-public class TombstoneMixin {
-    @Inject(method="onDeath", at=@At("TAIL"), cancellable = true)
-    public void tombstoneOnDeath(DamageSource damageSource, CallbackInfo ci) {
-        // TODO
-        //BlockPos pos = Objects.requireNonNull(damageSource.getAttacker()).getBlockPos();
-        //World world = damageSource.getAttacker().getWorld();
-        //if(ci.)
-        System.out.println("Works");
-        //if(!world.isClient()){
-        //}
-        //BlockState stateInBlock = world.getBlockState(pos);
-        //BlockState state = stateInBlock.isOf(ModBlocks.TOMBSTONE) ? stateInBlock : ModBlocks.TOMBSTONE.getDefaultState();
-        //world.setBlockState(pos, state);
+@Mixin(ServerPlayerEntity.class)
+public abstract class TombstoneMixin {
+    @Inject(at = @At(value = "HEAD"), method = "onDeath")
+    private void onPlayerDeath(DamageSource source, CallbackInfo info) {
+        ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
+        Inventory inv = player.getInventory();
+        BlockPos pos = player.getBlockPos();
+        World world = player.getWorld();
+        BlockState stateInBlock = world.getBlockState(pos);
+        BlockState state = stateInBlock.isOf(ModBlocks.TOMBSTONE) ? stateInBlock : ModBlocks.TOMBSTONE.getDefaultState();
+        world.setBlockState(pos, state);
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof TombstoneBlockEntity tombstoneBlockEntity) {
+            for(int i=0;i<inv.size();i++){
+                tombstoneBlockEntity.setStack(i,inv.getStack(i));
+            }
+        }
+
+        MinecraftServer server = player.getServer();
+        assert server != null;
+        CommandManager commandManager = server.getCommandManager();
+        // `player` can be anything that can issue commands (i.e. a `CommandSource`)
+        PlayerEntity playerEntity = (PlayerEntity) (Object) this;
+        commandManager.execute(playerEntity, "clear "+player.getName());
     }
 }
+
