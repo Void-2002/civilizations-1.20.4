97f9695b3b22124a2844ee538fd09dc0baa7a17d
Void <filipecastro2002@gmail.com>
2025-02-08 23:33:17 +0000

    Finished tombstone

--- src/main/java/net/_void/civilizations/mixin/TombstoneMixin.java
+++ src/main/java/net/_void/civilizations/mixin/TombstoneMixin.java
@@ -5,28 +5,18 @@ import net._void.civilizations.block.ModBlocks;
 import net._void.civilizations.block.entity.TombstoneBlockEntity;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net._void.civilizations.block.custom.TombstoneBlock;
-
-import java.util.Objects;
 
 @Mixin(ServerPlayerEntity.class)
-public abstract class TombstoneMixin {
+public abstract class TombstoneMixin{
     @Inject(at = @At(value = "HEAD"), method = "onDeath")
     private void onPlayerDeath(DamageSource source, CallbackInfo info) {
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
@@ -41,14 +31,9 @@ public abstract class TombstoneMixin {
             for(int i=0;i<inv.size();i++){
                 tombstoneBlockEntity.setStack(i,inv.getStack(i));
             }
+            player.getInventory().clear();
         }
-
-        MinecraftServer server = player.getServer();
-        assert server != null;
-        CommandManager commandManager = server.getCommandManager();
-        // `player` can be anything that can issue commands (i.e. a `CommandSource`)
-        PlayerEntity playerEntity = (PlayerEntity) (Object) this;
-        commandManager.execute(playerEntity, "clear "+player.getName());
+        // TODO fix tombstone when player dies to explosion
     }
 }
 
