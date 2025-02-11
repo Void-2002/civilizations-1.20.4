bb18265c28dfb42f5e7403cd41ff7d805177f3a9
Void <filipecastro2002@gmail.com>
2025-02-11 17:31:29 +0000

    Added civilians to Egypt Completed advancements

--- src/main/java/net/_void/civilizations/datagen/ModRecipeProvider.java
+++ src/main/java/net/_void/civilizations/datagen/ModRecipeProvider.java
@@ -7,14 +7,20 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
-import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.Items;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.EnchantmentLevelEntry;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.*;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.recipe.ShapelessRecipe;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.util.Identifier;
 
 import java.util.List;
 import java.util.function.Consumer;
+
+import static net.minecraft.item.EnchantedBookItem.getEnchantmentNbt;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
     private static final List<ItemConvertible> SMOOTH_LIMESTONE_SMELTABLES = List.of(ModBlocks.LIMESTONE);
@@ -25,28 +31,28 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(RecipeExporter exporter) {
-        offerSmelting(exporter, SMOOTH_LIMESTONE_SMELTABLES, RecipeCategory.BUILDING_BLOCKS,ModBlocks.SMOOTH_LIMESTONE,
-                0.1f,200,"limestone");
+        offerSmelting(exporter, SMOOTH_LIMESTONE_SMELTABLES, RecipeCategory.BUILDING_BLOCKS, ModBlocks.SMOOTH_LIMESTONE,
+                0.1f, 200, "limestone");
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS,ModBlocks.LIMESTONE_BRICKS,4).
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.LIMESTONE_BRICKS, 4).
                 pattern("##").
                 pattern("##").
-                input('#',ModBlocks.SMOOTH_LIMESTONE).
-                criterion(hasItem(ModBlocks.SMOOTH_LIMESTONE),conditionsFromItem(ModBlocks.SMOOTH_LIMESTONE)).
+                input('#', ModBlocks.SMOOTH_LIMESTONE).
+                criterion(hasItem(ModBlocks.SMOOTH_LIMESTONE), conditionsFromItem(ModBlocks.SMOOTH_LIMESTONE)).
                 offerTo(exporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS,ModBlocks.LIMESTONE_PILLAR,2).
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.LIMESTONE_PILLAR, 2).
                 pattern("#").
                 pattern("#").
-                input('#',ModBlocks.SMOOTH_LIMESTONE).
-                criterion(hasItem(ModBlocks.SMOOTH_LIMESTONE),conditionsFromItem(ModBlocks.SMOOTH_LIMESTONE)).
+                input('#', ModBlocks.SMOOTH_LIMESTONE).
+                criterion(hasItem(ModBlocks.SMOOTH_LIMESTONE), conditionsFromItem(ModBlocks.SMOOTH_LIMESTONE)).
                 offerTo(exporter);
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, Items.BOOK,1).
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, Items.BOOK, 1).
                 input(Items.LEATHER).
-                input(ModItems.PAPYRUS,3).
-                criterion(hasItem(ModItems.PAPYRUS),conditionsFromItem(ModItems.PAPYRUS)).
-                criterion(hasItem(Items.LEATHER),conditionsFromItem(Items.LEATHER)).
+                input(ModItems.PAPYRUS, 3).
+                criterion(hasItem(ModItems.PAPYRUS), conditionsFromItem(ModItems.PAPYRUS)).
+                criterion(hasItem(Items.LEATHER), conditionsFromItem(Items.LEATHER)).
                 offerTo(exporter, new Identifier(getRecipeName(Items.BOOK) + "papyrus_book"));
     }
 }
