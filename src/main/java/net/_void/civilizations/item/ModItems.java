d757872303215609fe073bc1fc55e5a763244e3a
Void <filipecastro2002@gmail.com>
2025-03-09 21:22:12 +0000

    Added nordic structure

--- src/main/java/net/_void/civilizations/item/ModItems.java
+++ src/main/java/net/_void/civilizations/item/ModItems.java
@@ -24,7 +24,7 @@ public class ModItems {
     public static final Item EGYPT_CROOK = registerItem("egypt_crook", new EgyptCrook(3, -2.0F, new FabricItemSettings().maxCount(1).rarity(Rarity.EPIC)));
     public static final Item CHINA_KEY_FRAGMENT = registerItem("china_key_fragment", new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON)));
     public static final Item CHINA_KEY = registerItem("china_key", new ChinaKey(new FabricItemSettings().maxCount(1).rarity(Rarity.RARE)));
-    public static final Item CHINA_BATTLE_AXE = registerItem("china_battle_axe", new ChinaBattleAxe(9, -3.0F, new FabricItemSettings().maxCount(1).rarity(Rarity.EPIC)));
+    public static final Item CHINA_BATTLE_AXE = registerItem("china_battle_axe", new ChinaBattleAxe(6, -2.8F, new FabricItemSettings().maxCount(1).rarity(Rarity.EPIC)));
     public static final Item CHINA_COIN = registerItem("china_coin", new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON)));
     public static final Item CHINA_SWORD = registerItem("china_sword", new SwordItem(ToolMaterials.NETHERITE, 2, -1.5F, new FabricItemSettings().maxCount(1).rarity(Rarity.EPIC)));
     public static final Item HIGH_MOUNTAINS_AND_FLOWING_WATER_MUSIC_DISC = registerItem("high_mountains_music_disc",
